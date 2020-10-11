5b649fa74ce6ada4ec62aa407f474bc9b87f27cb
rjdaw <daw19003@byui.edu>
2020-10-10 23:12:42 -0500

    preston

--- lesson4/script/javascript.js
+++ lesson4/script/javascript.js
@@ -1,5 +1,15 @@
 let year =  new Date().getFullYear();
 document.getElementById("currentYear").innerHTML = year;
 
-let modif = document.lastModified;
-document.getElementById("currentDate").innerHTML = modif;
+let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+};
+
+document.getElementById("currentDate").innerHTML = new Date().toLocaleDateString("en-GB", options);
+
+function toggleMenu () {
+    document.getElementById("responsive-nav").classList.toggle("hide");
+}
