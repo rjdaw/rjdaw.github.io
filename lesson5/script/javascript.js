e85cd8efafbf23ac0aece204afc9fe03d124534f
rjdaw <daw19003@byui.edu>
2020-10-18 00:11:32 -0500

    pancakes

--- lesson5/script/javascript.js
+++ lesson5/script/javascript.js
@@ -1,6 +1,8 @@
+//gets the current year
 let year =  new Date().getFullYear();
 document.getElementById("currentYear").innerHTML = year;
 
+//takes care of the current date
 let options = {
     weekday: "long",
     day: "numeric",
@@ -10,6 +12,17 @@ let options = {
 
 document.getElementById("currentDate").innerHTML = new Date().toLocaleDateString("en-GB", options);
 
+//checks if today is Friday
+var currentDate = new Date();
+var dayOfWeek = currentDate.getDay();
+
+if (dayOfWeek == 5) {
+    document.getElementById("pancakes").style.display = "block";
+} else {
+    document.getElementById("pancakes").style.display = "none";
+}
+
+//toggles menu
 function toggleMenu () {
     document.getElementById("responsive-nav").classList.toggle("hide");
 }
