1842a1ae0b5b25a5b38e1a123d32f8838210359a
rjdaw <daw19003@byui.edu>
2020-10-25 14:03:53 -0600

    lesson6

---
+++ lesson6/script/javascript.js
@@ -0,0 +1,28 @@
+//gets the current year
+let year =  new Date().getFullYear();
+document.getElementById("currentYear").innerHTML = year;
+
+//takes care of the current date
+let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+};
+
+document.getElementById("currentDate").innerHTML = new Date().toLocaleDateString("en-GB", options);
+
+//checks if today is Friday
+var currentDate = new Date();
+var dayOfWeek = currentDate.getDay();
+
+if (dayOfWeek == 5) {
+    document.getElementById("pancakes").style.display = "block";
+} else {
+    document.getElementById("pancakes").style.display = "none";
+}
+
+//toggles menu
+function toggleMenu () {
+    document.getElementById("responsive-nav").classList.toggle("hide");
+}
