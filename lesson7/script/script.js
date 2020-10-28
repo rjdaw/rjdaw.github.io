d938cddc6bec3452c97c5fd30fa6cef5291e3df7
rjdaw <daw19003@byui.edu>
2020-10-28 12:12:47 -0600

    lazy options

--- lesson7/script/script.js
+++ lesson7/script/script.js
@@ -1,4 +1,10 @@
 let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+let imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 100px 0px"
+};
+
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
   image.onload = () => {
@@ -6,6 +12,19 @@ const loadImages = (image) => {
   };
 };
 
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-});
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (!item.isIntersecting) {
+                return;
+            } else {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions)
+
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+} else {}
