04d551668b1fe667cfa7513c337f66a25c176a05
rjdaw <daw19003@byui.edu>
2020-10-28 12:23:17 -0600

    imgObserver

--- lesson7/script/script.js
+++ lesson7/script/script.js
@@ -1,8 +1,8 @@
 let imagesToLoad = document.querySelectorAll('img[data-src]');
 
 let imgOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px 100px 0px"
+    threshold: 1,
+    rootMargin: "0px 0px -500px 0px"
 };
 
 const loadImages = (image) => {
@@ -12,19 +12,17 @@ const loadImages = (image) => {
   };
 };
 
-if ('IntersectionObserver' in window) {
-    const imgObserver = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-            if (!item.isIntersecting) {
-                return;
-            } else {
-                loadImages(item.target);
-                imgObserver.unobserve(item.target);
-            }
-        });
-    }, imgOptions)
-
-    imagesToLoad.forEach((img) => {
-        loadImages(img);
+const imgObserver = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+        if (!item.isIntersecting) {
+            return;
+        } else {
+            loadImages(item.target);
+            imgObserver.unobserve(item.target);
+        }
     });
-} else {}
+}, imgOptions)
+
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+});
