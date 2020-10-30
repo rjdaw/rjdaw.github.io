c559c0dc7aa88dd099460503b1064458bfbd6f97
rjdaw <daw19003@byui.edu>
2020-10-29 18:43:43 -0600

    fixed js

--- lesson7/script/script.js
+++ lesson7/script/script.js
@@ -6,23 +6,30 @@ let imgOptions = {
 };
 
 const loadImages = (image) => {
-  image.setAttribute('src', image.getAttribute('data-src'));
-  image.onload = () => {
-    image.removeAttribute('data-src');
-  };
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
 };
 
-const imgObserver = new IntersectionObserver((items, observer) => {
-    items.forEach((item) => {
-        if (!item.isIntersecting) {
-            return;
-        } else {
-            loadImages(item.target);
-            imgObserver.unobserve(item.target);
-        }
-    });
-}, imgOptions)
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (!item.isIntersecting) {
+                return;
+            } else {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions)
 
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-});
+    imagesToLoad.forEach((image) => {
+        imgObserver.observe(image);
+    });
+
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
