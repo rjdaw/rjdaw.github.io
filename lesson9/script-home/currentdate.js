e4a357ccade9ef640048d22e220b726f05c8e586
rjdaw <daw19003@byui.edu>
2020-11-14 16:05:41 -0600

    lesson9-creation

---
+++ lesson9/script-home/currentdate.js
@@ -0,0 +1,9 @@
+//takes care of the current date
+let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+};
+
+document.getElementById("currentDate").innerHTML = new Date().toLocaleDateString("en-GB", options);
