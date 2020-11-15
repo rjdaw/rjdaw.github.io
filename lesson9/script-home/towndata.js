438a4b7dfddc584347b0e414812f7d59576e2fd5
rjdaw <daw19003@byui.edu>
2020-11-14 22:41:05 -0600

    medium

--- lesson9/script-home/towndata.js
+++ lesson9/script-home/towndata.js
@@ -9,13 +9,15 @@ fetch(requestURL)
     const town = jsonObject['towns'];
     const filteredTowns = town.filter(town => (town.name == 'Preston' || town.name == 'Soda Springs' || town.name == 'Fish Haven'));
     for (let i = 0 ; i < filteredTowns.length; i++) {  
-        let towncard = document.createElement('section');
-        let townname = document.createElement('h2');
-        let motto = document.createElement('p');
-        let founded = document.createElement('p');
+        let towncard   = document.createElement('section');
+        let townname   = document.createElement('h2');
+        let motto      = document.createElement('h3');
+        let founded    = document.createElement('p');
         let population = document.createElement('p');
-        let rain = document.createElement('p');
-        let image = document.createElement('img');
+        let rain       = document.createElement('p');
+        let image      = document.createElement('img');
+        let towncardtext   = document.createElement("div");
+        let towncardimg    = document.createElement("div");
 
         townname.textContent = filteredTowns[i].name;
         motto.textContent = filteredTowns[i].motto;
@@ -24,13 +26,17 @@ fetch(requestURL)
         rain.textContent = "Annual Rainfall: " + filteredTowns[i].averageRainfall;
         image.setAttribute('src', "images/" + filteredTowns[i].photo);
         image.setAttribute('alt', townname.textContent);
+        towncardtext.setAttribute('class', "towncardtext");
+        towncardimg.setAttribute('class', "towncardimg")
 
-        towncard.appendChild(townname);
-        towncard.appendChild(motto);
-        towncard.appendChild(founded);
-        towncard.appendChild(population);
-        towncard.appendChild(rain);
-        towncard.appendChild(image);
+        towncardtext.appendChild(townname);
+        towncardtext.appendChild(motto);
+        towncardtext.appendChild(founded);
+        towncardtext.appendChild(population);
+        towncardtext.appendChild(rain);
+        towncard.appendChild(towncardtext)
+        towncardimg.appendChild(image);
+        towncard.appendChild(towncardimg)
 
         document.querySelector('.town-cards').appendChild(towncard);
     }
