1f57123c68e5885a7dea5009dcd5c4ddd15b7c9c
rjdaw <daw19003@byui.edu>
2020-11-21 15:48:23 -0600

    remove-useless

--- lesson10/script/weatherapi.js
+++ lesson10/script/weatherapi.js
@@ -8,12 +8,6 @@ fetch(apiURL)
         document.getElementById('temp').textContent = Math.round(jsObject.main.temp);
         document.getElementById('humidity').textContent = jsObject.main.humidity;
         document.getElementById('speed').textContent = jsObject.wind.speed;
-
-        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
-        const desc = jsObject.weather[0].description; // note how we reference the weather array
-        document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
-        document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
-        document.getElementById('icon').setAttribute('alt', desc);
     });
 
 const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=af075121afb12f2ae54562fadc9117a2&units=imperial";
