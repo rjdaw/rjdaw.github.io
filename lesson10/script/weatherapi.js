3b37a5d6f76742dc9b8cdf056121a49d68b10327
rjdaw <daw19003@byui.edu>
2020-11-21 16:04:16 -0600

    no-console

--- lesson10/script/weatherapi.js
+++ lesson10/script/weatherapi.js
@@ -3,7 +3,6 @@ const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        console.log(jsObject);
         document.getElementById('desc').textContent = jsObject.weather[0].description;
         document.getElementById('temp').textContent = Math.round(jsObject.main.temp);
         document.getElementById('humidity').textContent = jsObject.main.humidity;
