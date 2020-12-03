119f4d959a5e049ff40998b195385a5fa2a51ac4
rjdaw <daw19003@byui.edu>
2020-12-02 19:44:14 -0600

    windchill

--- lesson11/script/windchill.js
+++ lesson11/script/windchill.js
@@ -3,6 +3,8 @@ let temp      = parseFloat(document.getElementById("temp").innerText);
 let speed     = parseFloat(document.getElementById("speed").innerText);
 let windChill = parseInt(35.74 + 0.6215 * temp - 35.75 * speed**.16 + .4275 * temp * speed**.16);
 
+console.log(temp, speed);
+
 if (temp <= 50 && speed > 3) {
     document.getElementById("chill").innerHTML = windChill.toFixed(2) + '&#176;F';
 
